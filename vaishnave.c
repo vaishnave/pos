1e833ca2b7a1933d1c085d1da482bfb3f847b98e
vaishnave <vaishnavekpr13@gmail.com>
2016-07-08 16:06:58 +0530

    Update and rename README.md to vaishnave.c

---
+++ vaishnave.c
@@ -0,0 +1,21 @@
+# pos
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number:");
+scanf("%d",&a);
+if(a=0)
+{
+printf("boundary");
+}
+elseif(a>0)
+{
+printf("positive number");
+}
+elseif(a<0)
+{
+printf("negative number");
+}
+}
+}
